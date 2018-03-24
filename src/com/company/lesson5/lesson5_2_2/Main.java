1890df8b2f3cffd8d560d9f6908ef2cc9b951d35
Anna <hannah8081998@gmail.com>
2018-03-24 17:21:41 +0200

    completed task lesson5_2_2

--- src/com/company/lesson5/lesson5_2_2/Main.java
+++ src/com/company/lesson5/lesson5_2_2/Main.java
@@ -2,6 +2,9 @@ package com.company.lesson5.lesson5_2_2;
 
 public class Main {
     public static void main(String[] args) {
-
+        Translator translator = new Translator();
+        translator.initDictionary();
+        translator.parseString("No, I worked in Dairy Queen.");
+        System.out.println(translator.translate());
     }
 }
