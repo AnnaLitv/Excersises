4ebe0280cd32a5d5285ce1e69f972f4dc60d3043
Anna <hannah8081998@gmail.com>
2018-04-11 20:42:55 +0300

    complete task 8_4

---
+++ src/main/java/com/company/lesson8/lesson8_4/Runner.java
@@ -0,0 +1,20 @@
+package com.company.lesson8.lesson8_4;
+
+public class Runner {
+
+
+    public void run(){
+        Lambda lambda = new Lambda();
+        lambda.initalize();
+        lambda.printAll();
+        lambda.sortAll();
+        System.out.println("");
+        lambda.printAll();
+        lambda.workWithMass(x->(x>5)?x:0);
+        System.out.println("");
+        char c = 's';
+        lambda.workWithList(x -> (x.charAt(0)==c)?true:false);
+        lambda.printAll();
+    }
+}
+
